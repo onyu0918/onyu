e856caf8242221e82e7d28b85b9cb674dad5f038
onyu0918 <onyu0918@naver.com>
2023-12-25 21:44:18 +0900

    devfox

---
+++ src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,18 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@RequiredArgsConstructor
+public class SampleHotel {
+	
+	private final Chef chef;
+	
+	
+	
+
+}
